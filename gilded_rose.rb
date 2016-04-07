7926e8cfd1b4ffdb32a05dbb82e1c5cc7daa7ae1
Bryan Cosgrove <ODNinja@gmail.com>
2016-04-06 23:44:19 -0400

    Update gilded_rose.rb

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,63 +1,96 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+class UpdateItem
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def call
+    item.quality += quality_modifier
+    item.sell_in += sell_in_modifier
+  end
+
+  private
+  def quality_modifier
+    return 0 if item.quality < 1
+    return -2 if item.sell_in < 1
+    -1
+  end
+
+  def sell_in_modifier
+    -1
   end
 end
 
-# DO NOT CHANGE THINGS BELOW -----------------------------------------
+class UpdateAgedBrie < UpdateItem
+
+  private
+  def quality_modifier
+    return 0 if item.quality >= 50
+    return 2 if item.sell_in < 1 && item.quality < 49
+    1
+  end
+end
+
+class UpdateLegendary < UpdateItem
+
+  private
+  def quality_modifier
+    0
+  end
+
+  def sell_in_modifier
+    0
+  end
+end
+
+class UpdateBackstagePasses < UpdateItem
+
+  private
+  def quality_modifier
+    return -item.quality if item.sell_in < 1
+    return 0 if item.quality >= 50
+    return 2 if (6..10).include? item.sell_in
+    return 3 if (1..5).include? item.sell_in
+    1
+  end
+end
+
+class UpdateConjured < UpdateItem
+
+  private
+  def quality_modifier
+    return 0 if item.quality < 1
+    return -4 if item.sell_in < 1
+    -2
+  end
+end
+
+class ItemUpdater
+  def self.call(item)
+    updater = case item.name
+    when "Aged Brie"
+      UpdateAgedBrie.new(item)
+    when "Sulfuras, Hand of Ragnaros"
+      UpdateLegendary.new(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      UpdateBackstagePasses.new(item)
+    when /Conjured/
+      UpdateConjured.new(item)
+    else
+      UpdateItem.new(item)
+    end
+
+    updater.call
+  end
+end
+
+def update_quality(items)
+  items.each do |item|
+    ItemUpdater.call(item)
+  end
+end
+
+######### DO NOT CHANGE BELOW #########
 
 Item = Struct.new(:name, :sell_in, :quality)
-
-# We use the setup in the spec rather than the following for testing.
-#
-# Items = [
-#   Item.new("+5 Dexterity Vest", 10, 20),
-#   Item.new("Aged Brie", 2, 0),
-#   Item.new("Elixir of the Mongoose", 5, 7),
-#   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-#   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-#   Item.new("Conjured Mana Cake", 3, 6),
-# ]
-
